cff8647a6ece8f0d9fbd10ccaf55fccbb4e25dcd
Rina.esp <kafka88@yandex.ru>
2022-06-13 15:36:37 +0300

    add file

--- src/test/java/ru/netology/tests/DeliveryCardTest.java
+++ src/test/java/ru/netology/tests/DeliveryCardTest.java
@@ -1,12 +1,7 @@
 package ru.netology.tests;
 
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 import java.time.LocalDate;
